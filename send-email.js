c62ecb6a5dee4d5e0455a4e81ed517d816d04b98
unknown <saran.senguttuvan@absyz.com>
2023-02-21 15:48:02 +0530

    Add existing project files to Git

---
+++ send-email.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+
+
+app.get('/', (req, res) => {
+  var options = {
+    root: path.join(__dirname)
+};
+ 
+
+var fileName = "rect.png";
+ 
+  console.log(req,"request");
+  const timestamp = new Date();
+  console.log(timestamp,"time");
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+        // next(err);
+    } else {
+        console.log('Sent:', fileName);
+        
+    }
+});
+  // Log the email open
+});
+app.listen(PORT, function(err){
+  console.log(`server started on port ${PORT}`);
+});
