8ca5c6272c0485cec983aa31a6dcb75c975f6d0c
unknown <saran.senguttuvan@absyz.com>
2023-03-03 12:10:44 +0530

    latest code

--- mailTracker.js
+++ mailTracker.js
@@ -1,33 +1,38 @@
-
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3080;
 
-app.get('/', (req, res) => {
+const { v4: uuidv4 } = require('uuid');
+const message_id = uuidv4();
+console.log(message_id,"msgid");
+
+
+app.get("/", (req, res) => {
   var options = {
-    root: path.join(__dirname)
-};
- 
-var fileName = "rect.png";
- 
-  // console.log(req,"request");
+    root: path.join(__dirname),
+  };
+
+  var fileName = "rect.png";
   const timestamp = new Date();
-  console.log(timestamp,"time");
+  console.log(timestamp, "time");
   res.sendFile(fileName, options, function (err) {
     if (err) {
-        // next(err);
+      // next(err);
     } else {
-        console.log('Sent:', fileName);
-        
+      console.log("Sent:", fileName);
     }
+  });
+
+  let email = req.query.email;
+  console.log(email,"email");
+
 });
-  // Log the email open
-});
-app.get('/test', (req, res, next)=>{
-  res.send("Testing");
-})
-app.listen(PORT, function(err){
+
+// app.get("/test", (req, res, next) => {
+//   res.send("Testing");
+// });
+
+app.listen(PORT, function (err) {
   console.log(`server started on port ${PORT}`);
 });
