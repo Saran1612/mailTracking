0cb1f82dea2944fbf62789b3e8c068b000c328b4
unknown <saran.senguttuvan@absyz.com>
2023-03-06 10:49:31 +0530

    latest

--- mailTracker.js
+++ mailTracker.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var mysql = require('mysql');
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3080;
@@ -25,12 +26,25 @@ app.get("/", (req, res) => {
   });
 
   let email = req.query.email;
+  let UID = req.query.uid;
   console.log(email,"email");
+  console.log(UID,"uid");
 
 });
 
-// app.get("/test", (req, res, next) => {
-//   res.send("Testing");
+app.get("/test", (req, res, next) => {
+  res.send("Testing");
+});
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Password@123"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
 // });
 
 app.listen(PORT, function (err) {
