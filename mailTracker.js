97cb9dafeac92d50cbf0a30c0d95e3bed70b944b
unknown <saran.senguttuvan@absyz.com>
2023-03-06 15:02:24 +0530

    latest

--- mailTracker.js
+++ mailTracker.js
@@ -2,6 +2,7 @@ const express = require("express");
 var mysql = require('mysql');
 const path = require("path");
 const app = express();
+var moment = require('moment');
 const PORT = process.env.PORT || 3080;
 
 
@@ -11,8 +12,7 @@ app.get("/", (req, res) => {
   };
 
   var fileName = "rect.png";
-  const timestamp = new Date();
-  console.log(timestamp, "time");
+  
   res.sendFile(fileName, options, function (err) {
     if (err) {
       // next(err);
@@ -21,10 +21,13 @@ app.get("/", (req, res) => {
     }
   });
 
+  const timestamp = new Date();
+  // console.log(timestamp, "time");
+  console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
   let email = req.query.email;
   let UID = req.query.msgId;
   console.log(email,"email");
-  console.log(UID,"uid");
+  console.log(req.query,"uid");
 
 });
 
@@ -32,11 +35,12 @@ app.get("/test", (req, res, next) => {
   res.send("Testing");
 });
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Password@123"
-// });
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Password@123",
+  database: 'your_database_name',
+});
 
 // con.connect(function(err) {
 //   if (err) throw err;
