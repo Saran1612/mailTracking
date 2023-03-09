4bb345a73f22bd9f9a08567c81bc7c8fc1d96999
unknown <saran.senguttuvan@absyz.com>
2023-03-09 11:36:48 +0530

    latest

--- mailTracker.js
+++ mailTracker.js
@@ -11,7 +11,7 @@ const db = require("./connection");
 async function MysqlQueryExecute(query1) {
   let finalresult = [];
   const [result] = await db.execute(query1);
-  console.log(JSON.stringify(result), "final result");
+  // console.log(JSON.stringify(result), "final result");
   // await db.execute(query1, async function (err, result) {
   // if (err) throw err;
   // else {
@@ -37,15 +37,16 @@ app.get("/", async (req, res) => {
   console.log("Opened At", timestamp);
   let email = req.query.email;
   let UID = req.query.msgId;
+  console.log(req.query);
   console.log(email, "email");
   console.log(UID, "uid");
   let countQuery = `SELECT COUNT(UQ_ID) AS MESSAGECount FROM Email_Tracking.MAIL_USER WHERE UQ_ID = 1 and USER_NAME = "TEST"`;
   let FinalCount = await MysqlQueryExecute(countQuery);
-  console.log(FinalCount, "message count");
+  // console.log(FinalCount, "message count");
   if (FinalCount.MESSAGECount !== 0) {
     let IncrmentQuery = `UPDATE Email_Tracking.MAIL_USER SET Count = 1 WHERE UQ_ID = 1 and USER_NAME = "TEST"`;
     let FinalResult = await MysqlQueryExecute(IncrmentQuery);
-    console.log(FinalResult, "Updated count");
+    // console.log(FinalResult, "Updated count");
   }
   res.sendFile(fileName, options, function async(err) {
     if (err) {
