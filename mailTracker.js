3e6acce0f8756dc697cd6acb7dc902549b8a2aa4
unknown <saran.senguttuvan@absyz.com>
2023-03-13 19:23:21 +0530

    latest

--- mailTracker.js
+++ mailTracker.js
@@ -40,28 +40,31 @@ app.get("/", async (req, res) => {
     root: path.join(__dirname),
   };
   const current_time = moment().tz('Asia/Kolkata').format("YYYY-MM-DD HH:mm:ss")
-console.log(current_time,"Check current date and time")
+// console.log(current_time,"Check current date and time")
 
   var fileName = "rect.png";
   const timestamp = new Date().getTime();
-  console.log("Opened At", timestamp);
+  // console.log("Opened At", timestamp);
   let email = req.query.email;
   let UID = req.query.msgId;
   let subject = req.query.subject;
   // console.log(req.query);
-  console.log(subject, "subject");
-  console.log(email, "email");
-  console.log(UID, "uid");
+  console.log(req,"requestANSWER");
+  console.log(res,"responseANSWER");
+
+  // console.log(subject, "subject");
+  // console.log(email, "email");
+  // console.log(UID, "uid");
   let countQuery = `SELECT COUNT(UQ_ID) AS MESSAGECount FROM Email_Tracking.MAIL_USER WHERE UQ_ID = "${UID}" and USER_NAME = "${email}"`;
   let FinalCount = await MysqlQueryExecute(countQuery);
-  console.log(FinalCount, "message count");
+  // console.log(FinalCount, "message count");
   if (FinalCount[0].MESSAGECount !== 0) {
     let IncrmentQuery = `UPDATE Email_Tracking.MAIL_USER SET Count = Count+1 WHERE UQ_ID = "${UID}" and USER_NAME = "${email}"`;
     let UserDataQuery = `Select ID as USERID from Email_Tracking.MAIL_USER where UQ_ID = "${UID}" and USER_NAME = "${email}"`;
 
     
     let FinalResult = await MysqlQueryExecute(IncrmentQuery);
-    console.log(FinalResult,"Check Final Count");
+    // console.log(FinalResult,"Check Final Count");
     let UserData = await MysqlQueryExecute(UserDataQuery);
     // console.log(UserData[0].USERID,"Check User Data");
     if(UserData[0].USERID){
@@ -74,10 +77,10 @@ console.log(current_time,"Check current date and time")
     let AddedUser = await MysqlQueryExecute(`INSERT INTO Email_Tracking.MAIL_USER(USER_NAME, UQ_ID,Mail_Subject, COUNT) VALUES("${email}","${UID}","${subject}",1)`);
 
     let AddedUserId = await MysqlQueryExecute(`Select ID as UserId from Email_Tracking.MAIL_USER where USER_NAME="${email}" and UQ_ID = "${UID}"`);
-    console.log(AddedUserId, "Added userId");
+    // console.log(AddedUserId, "Added userId");
     if(AddedUserId.length > 0){
       const current_time = moment().tz('Asia/Kolkata').format("YYYY-MM-DD HH:mm:ss")
-      console.log(current_time,"Check current date and time")
+      // console.log(current_time,"Check current date and time")
     let AddedTimeStamp = await MysqlQueryExecute(`INSERT INTO Email_Tracking.TimeStamp(User_TimeStamp, user_id) VALUES('${current_time}', ${AddedUserId[0].UserId})`)
     }
     // }
