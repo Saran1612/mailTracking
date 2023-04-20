4b579ffe137b9c88fae69e2d5aa963343c1eeb7b
AbhishekPoojary8 <abhishek.poojary@absyz.com>
2023-04-20 18:44:49 +0530

    Latest changes

--- Router/index.js
+++ Router/index.js
@@ -5,13 +5,20 @@ const TrackPixelHandler = require("../Handler/trackingPixelHandler")
 const TrackLinkHandler = require("../Handler/linkTrackHandler")
 const GetMessageHandler = require("../Handler/messageHandler")
 const recipientDataHandler = require("../Handler/recipientHandler");
-const linkTrackHandler = require("../Handler/linkTrackDataHandler")
+const linkTrackHandler = require("../Handler/linkTrackDataHandler");
+const rateLimit = require("express-rate-limit");
+
+// Set up rate limiter
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 10, // limit each IP to 10 requests per windowMs
+});
 
 
 /***All the routes */
 router.post("/RecipientDetails",CreateRecipientandMessage);
 router.get("/TrackingPixel", TrackPixelHandler);
-router.get("/TrackLink", TrackLinkHandler);
+router.get("/TrackLink",limiter, TrackLinkHandler);
 router.get("/GetMessageByDate", GetMessageHandler);
 router.get("/GetRecipients", recipientDataHandler);
 router.get("/getTrackerLinkData",linkTrackHandler )
