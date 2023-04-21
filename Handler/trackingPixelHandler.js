c2f634bdcc8b62b5b6a0a9202085b2c42d9aa7c0
AbhishekPoojary8 <abhishek.poojary@absyz.com>
2023-04-21 16:50:54 +0530

    Latest changes

--- Handler/trackingPixelHandler.js
+++ Handler/trackingPixelHandler.js
@@ -14,36 +14,36 @@ const TrackPixelHandler = async (req, res, next) => {
       and absyz_email_track.Mail_Message.Message_Unique_Key = "${UID}"`);
 
     if (MessageCount.TrackerCount === 0) {
-      let countQuery = `SELECT Recipient_id ,COUNT(Recipient_id) AS RecipientCount FROM absyz_email_track.Recipient_Details WHERE  RecipientEmail = "${email}"`;
-      let Recipient_Data = await MysqlQueryExecute(countQuery);
-      if (Recipient_Data[0].RecipientCount === 0) {
-        const currentDateTimeString = moment().format("YYYY-MM-DD HH:mm:ss");
-        const recipientResult = await MysqlQueryExecute(
-          `INSERT INTO absyz_email_track.Recipient_Details (RecipientEmail) VALUES ("${email}")`
-        );
-        const CreateMessage = await MysqlQueryExecute(
-          `INSERT INTO absyz_email_track.Mail_Message (User_Id, Message_Subject, Message_Unique_Key, Sent_Time) VALUES (${recipientResult.insertId}, "${subject}", "${UID}", "${currentDateTimeString}")`
-        );
+    //   let countQuery = `SELECT Recipient_id ,COUNT(Recipient_id) AS RecipientCount FROM absyz_email_track.Recipient_Details WHERE  RecipientEmail = "${email}"`;
+    //   let Recipient_Data = await MysqlQueryExecute(countQuery);
+    //   if (Recipient_Data[0].RecipientCount === 0) {
+    //     const currentDateTimeString = moment().format("YYYY-MM-DD HH:mm:ss");
+    //     const recipientResult = await MysqlQueryExecute(
+    //       `INSERT INTO absyz_email_track.Recipient_Details (RecipientEmail) VALUES ("${email}")`
+    //     );
+    //     const CreateMessage = await MysqlQueryExecute(
+    //       `INSERT INTO absyz_email_track.Mail_Message (User_Id, Message_Subject, Message_Unique_Key, Sent_Time) VALUES (${recipientResult.insertId}, "${subject}", "${UID}", "${currentDateTimeString}")`
+    //     );
         
-        const TrackerData = await MysqlQueryExecute(
-          `INSERT INTO absyz_email_track.Tracker (user_id, message_id, Time_Stamp, Count) VALUES(${recipientResult.insertId}, ${CreateMessage.insertId}, "${currentDateTimeString}", 1)`
-        );
-      } else {
-        const [recipientResult] = await MysqlQueryExecute(
-          `Select Recipient_id as RecipientsId from absyz_email_track.Recipient_Details where RecipientEmail = "${email}"`
-        );
-        console.log(recipientResult.RecipientsId, "Check recp");
-        const currentDateTimeString = moment().format("YYYY-MM-DD HH:mm:ss");
-        console.log(currentDateTimeString, "currentDateTimeString");
-        const CreateMessage = await MysqlQueryExecute(
-          `INSERT INTO absyz_email_track.Mail_Message (User_Id, Message_Subject, Message_Unique_Key, Sent_Time) VALUES (${recipientResult.RecipientsId}, "${subject}", "${UID}", "${currentDateTimeString}")`
-        );
-        console.log(CreateMessage.insertId, "Check created Message");
+    //     const TrackerData = await MysqlQueryExecute(
+    //       `INSERT INTO absyz_email_track.Tracker (user_id, message_id, Time_Stamp, Count) VALUES(${recipientResult.insertId}, ${CreateMessage.insertId}, "${currentDateTimeString}", 1)`
+    //     );
+    //   } else {
+    //     const [recipientResult] = await MysqlQueryExecute(
+    //       `Select Recipient_id as RecipientsId from absyz_email_track.Recipient_Details where RecipientEmail = "${email}"`
+    //     );
+    //     console.log(recipientResult.RecipientsId, "Check recp");
+    //     const currentDateTimeString = moment().format("YYYY-MM-DD HH:mm:ss");
+    //     console.log(currentDateTimeString, "currentDateTimeString");
+    //     const CreateMessage = await MysqlQueryExecute(
+    //       `INSERT INTO absyz_email_track.Mail_Message (User_Id, Message_Subject, Message_Unique_Key, Sent_Time) VALUES (${recipientResult.RecipientsId}, "${subject}", "${UID}", "${currentDateTimeString}")`
+    //     );
+    //     console.log(CreateMessage.insertId, "Check created Message");
         
-        const TrackerData = await MysqlQueryExecute(
-          `INSERT INTO absyz_email_track.Tracker (user_id, message_id, Time_Stamp, Count) VALUES(${recipientResult.RecipientsId}, ${CreateMessage.insertId}, "${currentDateTimeString}", 1)`
-        );
-      }
+    //     const TrackerData = await MysqlQueryExecute(
+    //       `INSERT INTO absyz_email_track.Tracker (user_id, message_id, Time_Stamp, Count) VALUES(${recipientResult.RecipientsId}, ${CreateMessage.insertId}, "${currentDateTimeString}", 1)`
+    //     );
+    //   }
     } else {
       const TrackerCountInc =
         await MysqlQueryExecute(`UPDATE absyz_email_track.Tracker
